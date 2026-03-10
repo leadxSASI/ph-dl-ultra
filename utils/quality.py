5b2c8cf3b27884fa5abd4a0e0c5e201d4c9c18ae
leadxSASI <nilanthirenuka106@gmail.com>
2026-03-10 08:38:49 +0530

    Delete utils directory

--- utils/quality.py
+++
@@ -1,8 +0,0 @@
-def get_quality_options():
-    return {
-        "4K": "best[height<=2160]",
-        "1080p": "best[height<=1080]",
-        "720p": "best[height<=720]",
-        "480p": "best[height<=480]",
-        "Audio Only": "bestaudio"
-    }
